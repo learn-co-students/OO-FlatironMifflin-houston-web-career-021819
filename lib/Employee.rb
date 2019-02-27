9641b3a0574d4ef00930a8407cecf2abd8a96a79
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-02-27 00:57:54 -0600

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,56 @@
 class Employee
+    attr_accessor :name, :salary, :manager, :department
+    @@all = []
+    def initialize(name, salary=0, department)
+        self.name = name
+        self.salary = salary
+        self.manager = manager
+        self.department = department
+        
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+    
+    def manager
+        @manager
+     end
+
+    def department
+        @department = department
+    end
+
+    def self.paid_over(amount)
+        over_paid_employees = []
+        @@all.select do |employee|
+            if employee.salary > amount
+                over_paid_employees << employee.name 
+            end
+        end
+        over_paid_employees
+    end
+
+    def self.find_by_department(dept)
+        manager_of_that_dept = " "
+        first_employee = " "
+        Manager.all.each do |manager|
+            if manager.department == dept
+                manager_of_that_dept = manager
+            end
+            first_employee = manager_of_that_dept.employees.first
+        end
+        
+    end
+
+    def tax_bracket
+        result = []
+        @@all.select do |employee|
+            if (employee.salary==self.salary+1000)||(employee.salary==self.salary-1000)
+                result << employee.name 
+            end
+        end
+     result
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,59 @@
 class Manager
+    attr_accessor :name, :age, :department, :employees
+
+    @@all = []
+    
+    def initialize(name, department, age=0, employees=[])
+        self.name = name
+        self.department = department
+        self.age = age
+        self.employees = employees
+        @@all << self
+    end
+
+    # def name
+    #     @name
+    # end
+
+    # def department
+    #     @department
+    # end
+
+    # def age
+    #     @age
+    # end
+
+    def employees
+        @employees.map do |employee|
+            employee
+        end
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def hire_employee(employee_name, employee_salary)
+        new_employee = Employee.new(employee_name, employee_salary)
+        @employees << self.employee_name
+        employee_name.manager = self
+    end
+
+    def self.all_departments
+        @@all.map do |manager|
+            manager.department
+        end
+    end
+
+    def self.average_age 
+        total = 0
+        average = 0.00
+        @@all.each do |manager|
+            total += manager.age
+        end
+        average = (total.to_f/@@all.length)
+
+    end 
+    
 
 end
