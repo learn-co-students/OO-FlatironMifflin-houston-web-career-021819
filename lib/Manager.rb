86fb95785d831a9b34820f07b371a0f28fab59f6
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-02-26 20:06:38 -0600

    done

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Manager
+  attr_accessor :name, :department, :age
+  @@all = []
+  def initialize(name, department, age)
+    self.name = name
+    self.department = department
+    self.age = age
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+
+  def hire_employee(name, salary)
+    Employee.new(name, salary) 
+  end
+
+  def self.all_departments
+    depart_array = [] 
+    self.all.each do |x|
+      depart_array << x.department
+    end
+  end
+
+  def self.average_age
+    manager_ages = []
+    self.all.each do |manager|
+      manager_ages << manager.age
+    end
+    manager_ages.inject {|sum, x| sum + x} / manager_ages.size
+  end
 
 end
