0496a47ce243ddaade0c35f770cb78183b803ad9
github username <github email address>
2019-02-28 21:33:19 -0600

    Done

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,36 @@
 class Manager
+   attr_accessor :name, :age, :department
 
+   @@all = []
+
+   def initialize(name, age, department)
+      @name = name
+      @department = department
+      @age = age
+      @@all << self
+    end
+
+    def self.all
+       @@all
+    end
+
+    def employees
+        Employee.all.select do |employee|
+            employee.manager == self
+        end
+    end
+
+    def hire_employee(name, salary)
+        self.employees << Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        @@all.map {|manager| manager.department}
+    end
+
+    def self.average_age
+        total = 0
+        @@all.map {|manager| total += manager.age}
+        (total / @@all.length).to_f
+    end
 end
--- run.rb
+++ run.rb
@@ -1,10 +1,20 @@
 require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
-
-
 #Test your code here
 
+mgr1 = Manager.new("Sam", 26, "Marketing")
+mgr2 = Manager.new("Steve", 30, "Sales")
+mgr3 = Manager.new("Jane", 35, "Admissions")
 
+emp1 = Employee.new("emp1", 8500, mgr1)
+emp2 = Employee.new("emp2", 6500, mgr2)
+emp3 = Employee.new("emp3", 9000, mgr3)
+emp4 = Employee.new("emp4", 12000, mgr1)
+find_by_department = Employee.find_by_department("Marketing")
+
+tax_bracket = emp3.tax_bracket
+
+hire_employee = mgr2.hire_employee("Jacob", 8000)
 binding.pry
 puts "done"
